56081fbfea9fe6bcf7b424fc0f6f6dbb63b5cbcc
Jyothi J <jasthijyothi@gmail.com>
2025-04-22 21:50:57 +0530

    added comment

--- src/main/java/com/faang/groceryStore/GroceryStoreApplication.java
+++ src/main/java/com/faang/groceryStore/GroceryStoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GroceryStoreApplication {
-
+// jyothi
 	public static void main(String[] args) {
 		SpringApplication.run(GroceryStoreApplication.class, args);
 	}
